a5dcb933ed86c072b83d95cb2969b6138af5f8fe
Aaron Nguyen <me@aaronnguyen.dev>
2023-09-13 18:55:34 -0400

    Add comments

--- discussion3/main.c
+++ discussion3/main.c
@@ -4,22 +4,27 @@
 char *helper(char *str, int left, int right)
 {
     int len = right - left + 1;
-    if (len == 0) // base case #1
+    if (len == 0) // base case #1: return empty string if nothing to sort
         return "";
     if (len == 1)
-    { // base case #2
-        char *rv = (char *)malloc(2);
-        strncpy(rv, str + left, 1);
+    {                                 // base case #2: return string with 1 character
+        char *rv = (char *)malloc(2); // malloc(2) to preserve for '\0' null terminator
+        strncpy(rv, str + left, 1);   // copy 1 character to rv
         return rv;
     }
-    // recurisve step!
-    char *first = helper(str, left, left + len / 2 - 1);
-    char *second = helper(str, left + len / 2, right);
-    char *rv = (char *)malloc(len + 1);
-    strcpy(rv, second);
-    strcat(rv, first);
+    // recursive step!
+    char *first = helper(str, left, left + len / 2 - 1); // recurse left-half
+    char *second = helper(str, left + len / 2, right);   // recurse right-half
+    char *rv = (char *)malloc(len + 1);                  // create temp array
+    strcpy(rv, second);                                  // copy second array to rv
+    strcat(rv, first);                                   // concat first array to rv starting at '\0'
+    // example:
+    // 1. rv = "", second = "second", first = "first"
+    // 2. rv = "second", first = "first"
+    // 3. rv = "secondfirst"
     return rv;
 }
+
 char *unknown_operation(char *str)
 {
     int len = strlen(str);
