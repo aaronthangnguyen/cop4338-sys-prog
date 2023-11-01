a4c4916dc1e093f76e2713879480f67df7e968db
Aaron Nguyen <me@aaronnguyen.dev>
2023-11-01 11:34:36 -0400

    Discussion 8 ✅

---
+++ discussion8/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX_SIZE 1000
+#define MAX_LEN 1000
+
+void print_txt(char *str)
+{
+    int num = 0;
+    for (int i = 0; i < strlen(str); i++)
+    {
+        if (str[i] == ',')
+        {
+            printf("%-25e\t", (double)num);
+            num = 0;
+        }
+        else
+        {
+            num = num * 10 + (str[i] - '0');
+        }
+    }
+    printf("\n");
+}
+
+int main()
+{
+    char csv[MAX_SIZE][MAX_LEN];
+
+    int i;
+    for (i = 0; i < MAX_SIZE; i++)
+    {
+        if (fgets(csv[i], sizeof(csv[i]), stdin) == NULL)
+            break;
+
+        int len = strlen(csv[i]);
+        if (len > 0 && csv[i][len - 1] == '\n')
+            csv[i][len - 1] = '\0';
+    }
+
+    int size = i;
+    for (int j = 0; j < size; j++)
+    {
+        print_txt(csv[j]);
+    }
+
+    return 0;
+}
