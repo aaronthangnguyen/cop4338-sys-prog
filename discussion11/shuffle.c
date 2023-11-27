aaf096884da5c92ea44a8ef077aff9542702288b
Aaron Nguyen <me@aaronnguyen.dev>
2023-11-27 15:10:23 -0500

    Discussion 11 ✅

--- discussion11/shuffle.c
+++ discussion11/shuffle.c
@@ -14,15 +14,15 @@ int main(int argc, char *argv[])
     long file_size = lseek(fd, 0, SEEK_END); // find the file size
     fprintf(stderr, "The file size is %ld bytes!\n", file_size);
     lseek(fd, 0, SEEK_SET); // rewind
-    for (long i = 0; i < file_size - 1; i++)
+    for (long i = 0; i < file_size / 2; i++)
     {
-        long loc = lrand() % (file_size - i) + i; // random number from i to file_size - 1
-        fprintf(stderr, "Swapped chars at index %ld and %ld.\n", i, loc);
+        long j = file_size - 1 - i;
+        fprintf(stderr, "Swapped chars at index %ld and %ld.\n", i, j);
         char from, to;
         read(fd, &from, 1);
-        lseek(fd, loc, SEEK_SET);
+        lseek(fd, j, SEEK_SET);
         read(fd, &to, 1);
-        lseek(fd, loc, SEEK_SET);
+        lseek(fd, j, SEEK_SET);
         write(fd, &from, 1);
         lseek(fd, i, SEEK_SET);
         write(fd, &to, 1);
